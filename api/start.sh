208addbff35125706d020b4ba51e1428c8b72fe4
Sergey Kozlov <serg@zillent.ru>
2020-06-20 02:11:06 +0500

    changed port

--- api/start.sh
+++ api/start.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 8811:8811 -e "DATABASE_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')" -v $(pwd)/media:/hack2/media -d hack2_api
+sudo docker run -p 8822:8822 -e "DATABASE_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')" -v $(pwd)/media:/hack2/media -d hack2_api
