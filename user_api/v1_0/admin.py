824985068fbce0f8fd7ced56afd489631501e40c
Sergey Kozlov <serg@zillent.ru>
2020-06-20 20:53:20 +0500

    added group and department to admin

--- user_api/v1_0/admin.py
+++ user_api/v1_0/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from v1_0.models import (
     Log, 
-    Person
+    Person, Group, Department
 )
 
 # Register your models here.
 
 admin.site.register(Log)
 admin.site.register(Person)
+
+admin.site.register(Group)
+admin.site.register(Department)
 
